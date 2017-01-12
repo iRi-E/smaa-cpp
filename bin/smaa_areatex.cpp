913fb46f8cd759f28ce494613618e13e1d8910e7
IRIE Shinsuke <irieshinsuke@yahoo.co.jp>
2017-01-13 02:00:26 +0900

    Bump version to 0.4.0

--- bin/smaa_areatex.cpp
+++ bin/smaa_areatex.cpp
@@ -21,7 +21,7 @@
  */
 
 /*
- * smaa_areatex.cpp  version 0.3.2
+ * smaa_areatex.cpp  version 0.4.0
  *
  * This is a part of smaa-cpp that is an implementation of
  * Enhanced Subpixel Morphological Antialiasing (SMAA) written in C++.
